a30e4a9f5e371e699dd1244563a99447554fa9c1
Kurt Madsen <kurt@crowdbotics.com>
2022-11-09 16:10:36 -0800

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "huthut_espn_pilot_p_37548",
   url: "https://huthut_espn_pilot_p_37548.botics.co",
   api: "https://huthut_espn_pilot_p_37548.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
